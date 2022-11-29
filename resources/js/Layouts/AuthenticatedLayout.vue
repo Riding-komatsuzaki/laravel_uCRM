6081fca61b634f14ea089bb5623ac3ec2e658c22
kalpee <shogo.galleria.game@gmail.com>
2022-11-29 23:34:26 +0900

    itemの下準備

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -20,9 +20,9 @@ const showingNavigationDropdown = ref(false);
                         <div class="flex">
                             <!-- Logo -->
                             <div class="shrink-0 flex items-center">
-                                <Link :href="route('dashboard')">
+                                <Link class="" :href="route('dashboard')">
                                     <ApplicationLogo
-                                        class="block h-9 w-auto fill-current text-gray-800"
+                                        class="block h-9 w-12 fill-current text-gray-800"
                                     />
                                 </Link>
                             </div>
